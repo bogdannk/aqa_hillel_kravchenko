cd82ed039f5412288d11ae7790f65cddeb506aec
Kravchenko Bogdan <kravchenko.appleid@gmail.com>
2024-07-21 17:37:33 +0300

    done: added lesson_01 folder and homework_01 file

---
+++ lesson_01/homework_01.py
@@ -0,0 +1,81 @@
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
+
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+
+# task 05 == виправте назви змінних
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
+
+
+"""
+    # Задачі 07 -10:
+    # Переведіть задачі з книги "Математика, 2 клас"
+    # на мову пітон і виведіть відповідь, так, щоб було
+    # зрозуміло дитині, що навчається в другому класі
+"""
+# task 07
+"""
+У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+Скільки всього дерев посадили в саду?
+"""
+apple_trees = 4
+pear_trees = apple_trees + 5
+plum_trees = apple_trees - 2
+total_trees = apple_trees + pear_trees + plum_trees
+print(total_trees)
+
+# task 08
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+morning_t = 5
+afternoon_t = morning_t - 10
+evening_t = afternoon_t + 4
+print(evening_t)
+# task 09
+"""
+Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
+1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
+Скількі сьогодні дітей у театральному гуртку?
+"""
+boys_total = 24
+girls_total = boys / 2
+boys_today = boys - 1
+girls_today = girls - 2
+total_children_today = boys_today + girls_today
+print(total_children_today)
+# task 10
+"""
+Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
+а третя - як половина вартості першої та другої разом.
+Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
+"""
+first_book_price = 8
+second_book_price = first_book_price + 2
+third_book_price = (first_book_price + second_book_price) / 2
+total_price = first_book_price + second_book_price + third_book_price
+print(total_price)
